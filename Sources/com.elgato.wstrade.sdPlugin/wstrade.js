18c5fc9b2bd56c6f957c00bf9d0c0cb14b42b62d
Nadeem Haidar <nadeem_haidar@hotmail.com>
2021-05-31 02:12:14 -0400

    display account value given oauth token

--- Sources/com.elgato.wstrade.sdPlugin/wstrade.js
+++ Sources/com.elgato.wstrade.sdPlugin/wstrade.js
@@ -1,19 +1,17 @@
 const baseUrl = "https://trade-service.wealthsimple.com"
 var WsTrade = {
-    getAccounts: function() {
-        fetch(baseUrl + "/account/list", {
-            headers: {"Authorization": "bearer placeholder"}
+    getAccounts: function(oauthToken, oauthToken) {
+        return fetch(baseUrl + "/account/list", {
+            headers: {"Authorization": "bearer " + oauthToken}
         })
             .then(response => response.json())
-            .then(data => console.log(data));
     },
-    accountHistory: function(accountId, interval) {
-        fetch(baseUrl + "/account/history/" + interval + "?" + new URLSearchParams({
+    accountHistory: function(oauthToken, accountId, interval) {
+        return fetch(baseUrl + "/account/history/" + interval + "?" + new URLSearchParams({
             "account_id": accountId
         }), {
-            headers: {"Authorization": "bearer placeholder"}
+            headers: {"Authorization": "bearer " + oauthToken}
         })
             .then(response => response.json())
-            .then(data => console.log(data));
     }
 }
