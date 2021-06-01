7d34b5f3655900df8d065595bd41439cb32cdd50
Nadeem Haidar <nadeem_haidar@hotmail.com>
2021-06-01 18:27:27 -0400

    move pi js to separate js file

---
+++ Sources/com.elgato.wstrade.sdPlugin/pi.js
@@ -0,0 +1,56 @@
+var pluginAction = null,
+uuid = '',
+aValueSlider = document.querySelectorAll('.setvalueSlider'),
+oValueSelector = document.querySelector(".setvalueSelect");
+
+if ($SD) {
+    $SD.on('connected', function (jsonObj) {
+        uuid = jsonObj['uuid'];
+        if (jsonObj.hasOwnProperty('actionInfo')) {
+            pluginAction = jsonObj.actionInfo['action'];
+        }
+    });
+};
+
+/** you can also use ES6 syntax like so:
+*
+*   if ($SD) $SD.on('connected', (jsonObj) => { uuid=jsonObj.uuid }));
+*    
+*/
+
+function sendValueToPlugin(value, param) {
+    // console.log("SENDING VALUE TO PLUGIN: ", value, uuid, pluginAction);
+    if (param === 'setValue') {
+
+        aValueSlider && Array.prototype.forEach.call(aValueSlider, function (ctl) {
+            ctl.value = value;
+        })
+
+        oValueSelector && Array.prototype.forEach.call(oValueSelector.options, function (o) {
+            const val = Math.round(value);
+            if (o.value !== val) {
+                oValueSelector.value = val;
+            }
+        })
+    }
+
+    if ($SD && $SD.connection) {
+        var payload = {};
+        if (param) {
+            payload[param] = value;
+        }
+        $SD.api.sendToPlugin(uuid, pluginAction, payload);
+    }
+}
+
+function oauthChange() {
+    document.getElementById("oauth-submit").disabled = !document.getElementById("oauth-input").checkValidity();
+}
+
+function oauthSubmit() {
+    let payload = {
+        "accountId": document.getElementById("account-id").value,
+        "oauthToken": document.getElementById("oauth-input").value
+    }
+    sendValueToPlugin(payload, 'pi-save-button');
+}
